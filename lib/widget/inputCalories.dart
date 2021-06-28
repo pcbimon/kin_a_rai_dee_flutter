d06675adcadabe021362b208a052a179374701c1
Patipat Chewprecha <patipat.che@mahidol.edu>
2021-06-28 19:31:20 +0700

    show modal with current data

--- lib/widget/inputCalories.dart
+++ lib/widget/inputCalories.dart
@@ -6,7 +6,7 @@ class inputCalories extends StatefulWidget {
   final double maxCalories;
   final double currentCalories;
   final Function setCalories;
-  inputCalories(this.maxCalories,this.currentCalories,this.setCalories);
+  inputCalories(this.maxCalories, this.currentCalories, this.setCalories);
 
   @override
   _inputCaloriesState createState() => _inputCaloriesState();
@@ -15,24 +15,29 @@ class inputCalories extends StatefulWidget {
 class _inputCaloriesState extends State<inputCalories> {
   final calController = TextEditingController();
   final _formKey = GlobalKey<FormState>();
-  String? validateCalories(double maxCal,String? currentCal){
-    if(currentCal != null && currentCal.isNotEmpty && double.parse(currentCal)>maxCal){
+  String? validateCalories(double maxCal, String? currentCal) {
+    if (currentCal != null &&
+        currentCal.isNotEmpty &&
+        double.parse(currentCal) > maxCal) {
       String maxStr = NumberFormat('#,###').format(maxCal);
       return 'Your calories is over $maxStr';
     }
     return null;
   }
+
   void submitData() {
     final enterCal = calController.text;
     widget.setCalories(double.parse(enterCal));
     Navigator.of(context).pop();
   }
+
   @override
   Widget build(BuildContext context) {
-    // calController.text = NumberFormat('####').format(widget.currentCalories).toString();
-    // calController.selection = TextSelection(
-    //     baseOffset: calController.text.length,
-    //     extentOffset: calController.text.length);
+    calController.text =
+        NumberFormat('####').format(widget.currentCalories).toString();
+    calController.selection = TextSelection(
+        baseOffset: calController.text.length,
+        extentOffset: calController.text.length);
     return Form(
       key: _formKey,
       child: Card(
@@ -45,18 +50,22 @@ class _inputCaloriesState extends State<inputCalories> {
                   decoration: InputDecoration(labelText: 'Selected Calories'),
                   keyboardType: TextInputType.number,
                   inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                  validator: (val){
-                    if(val != null && double.parse(val)>widget.maxCalories){
-                      String maxStr = NumberFormat('#,###').format(widget.maxCalories);
+                  validator: (val) {
+                    if (val != null && double.parse(val) > widget.maxCalories) {
+                      String maxStr =
+                          NumberFormat('#,###').format(widget.maxCalories);
                       return 'Your calories is over $maxStr';
                     }
                     return null;
                   },
                   controller: calController,
+                  onFieldSubmitted: (value) => {
+                    if (_formKey.currentState!.validate()) {submitData()}
+                  },
                 ),
                 ElevatedButton(
                     onPressed: () {
-                      if (_formKey.currentState!.validate()){
+                      if (_formKey.currentState!.validate()) {
                         submitData();
                       }
                     },
