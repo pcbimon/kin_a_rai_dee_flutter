e2b037fca4c50a2bce2e049f421ffc3a550203f5
Patipat Chewprecha <patipat.che@mahidol.edu>
2021-08-22 20:17:09 +0700

    Add Bottom Navigation Bar

--- lib/main.dart
+++ lib/main.dart
@@ -42,7 +42,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: [RandomFood()][_currentTab],
+      body: [RandomFood(), RandomFood(), RandomFood()][_currentTab],
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
         onTap: (index) => setState(() => _currentTab = index),
